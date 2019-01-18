49cf41dca252f70915ab81edfd0424072df58039
carpenter-js <carpenterbarrett@gmail.com>
2019-01-18 09:33:26 -0700

    created stack

---
+++ stack.js
@@ -0,0 +1,42 @@
+'use strict';
+
+class _Node {
+  constructor(data, next) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+  }
+
+  pop() {
+    const node = this.top;
+    this.top = node.next;
+    return node.data;
+  }
+
+  push(data) {
+    if (this.top === null) {
+      this.top = new _Node(data, null);
+      return this.top;
+    }
+
+    const node = new _Node(data, this.top);
+    this.top = node;
+  }
+
+}
+
+function main() {
+  const starTrek = new Stack();
+  starTrek.push('Kirk');
+  starTrek.push('Spock');
+  starTrek.push('McCoy');
+  starTrek.push('Scotty');
+  return starTrek;
+}
+
+console.log(main());
